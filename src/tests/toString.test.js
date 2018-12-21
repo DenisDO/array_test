ff4d77478254ba282427b2aae4d66c2c8791b724
Denis Derkach <podstavka128@gmail.com>
2018-12-21 16:39:33 +0200

    Fix all ESLint errors

--- src/tests/toString.test.js
+++ src/tests/toString.test.js
@@ -1,20 +1,18 @@
 import MyArray from '../index';
 
-
 describe('tests for method toString', () => {
-  let arr;
+  let arr = null;
 
   beforeEach(() => {
     arr = new MyArray(1, 2, 3, 4);
   });
-
 
   test('instance has method toString', () => {
     expect(arr.toString).toBeInstanceOf(Function);
   });
 
   test('instance has not Own Property toString', () => {
-    expect(arr.hasOwnProperty('toString')).toBeFalsy();
+    expect(Object.prototype.hasOwnProperty.call(arr, 'toString')).toBeFalsy();
   });
 
   test('the toString result of empty arr must be an empty string and not undefined', () => {
@@ -30,5 +28,5 @@ describe('tests for method toString', () => {
 
   test('returns a comma separated list of elements', () => {
     expect(arr.toString()).toBe('1,2,3,4');
-  })
+  });
 });
